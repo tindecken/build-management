0d79f20fb2debf1fc9ab5acd620f736f46d869d2
Nguyễn Thái Hoàng <tindecken@gmail.com>
2017-07-15 20:41:28 +0700

    Fist commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'build-management'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
